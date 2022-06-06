1eb0c6b31530d13ee1d5f4aef703b7cf8a40ec4e
Yuki Ito <mrno110y@gmail.com>
2022-06-06 21:54:10 +0900

    service: implement

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,7 +1,23 @@
-use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
+use regex::Regex;
 
 #[fastly::main]
-fn main(_req: Request) -> Result<Response, Error> {
-    Ok(Response::from_status(StatusCode::OK))
+fn main(mut req: Request) -> Result<Response, Error> {
+    // By default, disable caching for all requests.
+    req.set_pass(true);
+
+    let path = req.get_url().path();
+    let static_path_regex = Regex::new("^/static/.*$")?;
+
+    // Cache static assets.
+    if static_path_regex.is_match(path) {
+        req.set_pass(false);
+    }
+
+    let mut res = req.send("example.kauche.com")?;
+
+    // Add my-awesome-header HTTP header to the response
+    res.set_header("my-awesome-header", "my-awesome-value");
+
+    Ok(res)
 }
